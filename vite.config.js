5beb4cbb834e3f0315adb9640da3e24fa0d81f7a
jlimsy <limsy.joey@gmail.com>
2025-02-14 00:28:46 +0800

    remove react plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+// import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/virtual-museum/",
-  plugins: [react()],
+  plugins: [],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "/src"),
