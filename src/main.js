28a75f0a316eda81b623f6903d4451a0b6cc2f8c
jlimsy <limsy.joey@gmail.com>
2025-02-14 02:43:33 +0800

    add relative paths

--- src/main.js
+++ src/main.js
@@ -58,7 +58,8 @@ let position = 0;
 
 // gltf Loader
 const gltfLoader = new GLTFLoader();
-gltfLoader.load("/model/drawing-room/scene.gltf", (gltf) => {
+
+gltfLoader.load(" ./model/drawing-room/scene.gltf", (gltf) => {
   console.log("Our model here!", gltf);
   const model = gltf.scene;
   scene.add(model);
